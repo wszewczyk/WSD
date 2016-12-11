16769f373f90dc9cf1087d9db8f6c98928effc61
Radosław <r2dek93@gmail.com>
2016-12-11 16:27:32 +0100

    rdrozdz - add message component

---
+++ app/src/main/java/wsd/com/wsd/comunication/utils/Performative.java
@@ -0,0 +1,29 @@
+package wsd.com.wsd.comunication.utils;
+
+
+
+public enum Performative {
+    ACCEPT_PROPOSAL,
+    AGREEE,
+    CANCEL,
+    CFP,
+    CONFIRM,
+    DISCONFIRM,
+    FAILURE,
+    INFORM,
+    INFORM_IF,
+    INFORM_REF,
+    NOT_UNDERSTOOD,
+    PROPOSE,
+    QUERY_IF,
+    QUERY_REF,
+    REFUSE,
+    REJECT_PROPOSAL,
+    REQUEST,
+    REQUEST_WHEN,
+    REQUEST_WHENEVER,
+    SUBSCRIBE,
+    PROXY,
+    PROPAGATE,
+    UNNOWN;
+}
